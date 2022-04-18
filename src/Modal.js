f94890abdb52ad5e70fef6e3eee3e007021008c7
Revenant <dasdev.pratik@gmail.com>
2022-04-18 07:41:35 +0530

    Created a Navbar layout with Styled component

--- src/Modal.js
+++ src/Modal.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
+import styled from "styled-components";
 
-const MODAL_STYLES = {
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: '#FFF',
-  padding: '50px',
-  zIndex: 1000
-}
-
-const OVERLAY_STYLES = {
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: 'rgba(0, 0, 0, .7)',
-  zIndex: 1000
-}
+const ModalWrap = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 50px;
+  z-index: 1000;
+`;
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgb(0, 0, 0, 0.7);
+  z-index: 1000;
+`;
 
 export default function Modal({ open, children, onClose }) {
-  if (!open) return null
+  if (!open) return null;
 
   return ReactDom.createPortal(
     <>
-      <div style={OVERLAY_STYLES} />
-      <div style={MODAL_STYLES}>
+      <Overlay />
+      <ModalWrap>
         <button onClick={onClose}>Close Modal</button>
         {children}
-      </div>
-    </>,
+      </ModalWrap>
+    </>, 
     document.getElementById('portal')
-  )
+  );
 }
